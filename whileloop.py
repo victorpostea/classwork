c132225fa53aabf3607cb6800d92c861425e1832
victorpostea <60620574+victorpostea@users.noreply.github.com>
2020-03-09 12:28:30 -0400

    Create whileloop.py

---
+++ whileloop.py
@@ -0,0 +1,102 @@
+#Create a while loop that will print:
+    #1. from 419 to 647 by 6.
+    #2. from -207 to -207 by 0.
+    #3. from -475 to 133 by 19.
+    #4. from 310 to 490 by 9.
+    #5. from -342 to -146 by 4.
+    #6. from -103 to 313 by 16.
+    #7. from 439 to 439 by 0.
+    #8. from 128 to 154 by 1.
+    #9. from -251 to 199 by 10.
+    #10. from -45 to 772 by 19.
+
+runExample = int(input('Which Question?:'))
+
+if runExample == 1:
+    i = 419
+    while i <= 647:
+        print(i)
+        i += 6
+        if i == 647:
+            break
+
+if runExample == 2:
+    #2
+    i = -207
+    while i <= -207:
+        print(i)
+        i += 0
+        if i == -207:
+            break
+
+if runExample == 3:
+    #3
+    i = -475
+    while i <= 133:
+        print(i)
+        i += 19
+        if i == 133:
+            break
+
+if runExample == 4:
+    #4
+    i = 310
+    while i <= 490:
+        print(i)
+        i += 9
+        if i == 490:
+            break
+
+if runExample == 5:
+    #5
+    i = -342
+    while i <= -146:
+        print(i)
+        i += 4
+        if i == -146:
+            break
+
+if runExample == 6:
+    #6
+    i = -103
+    while i <= 313:
+        print(i)
+        i += 16
+        if i == 313:
+            break
+
+if runExample == 7:
+    #7
+    i = 439
+    while i <= 439:
+        print(i)
+        i += 0
+        if i == 439:
+            break
+
+if runExample == 8:
+    #8
+    i = 128
+    while i <= 154:
+        print(i)
+        i += 1
+        if i == 154:
+            break
+
+if runExample == 9:
+    #9
+    i = -251
+    while i <= 199:
+        print(i)
+        i += 10
+        if i == 199:
+            break
+
+if runExample == 10:
+    #10
+    i = -45
+    while i <= 772:
+        print(i)
+        i += 19
+        if i == 772:
+            break
